63cd6dc347d5a7d80e60e42aaebdc68e08e87640
jeff <jlgranof@icloud.com>
2022-07-06 14:57:11 -0500

    finished getting all the info from the api, just need to format it and loop thru all of em

--- pokeAPI.js
+++ pokeAPI.js
@@ -3,6 +3,8 @@ const fetch = require('node-fetch');
 const pokeUrl = 'https://pokeapi.co/api/v2/pokemon/'
 const speciesUrl = 'https://pokeapi.co/api/v2/pokemon-species/'
 
+// ADD IN A HELPER TO CAPITALIZE EVERY FIRST LETTER
+
 const getPokemon = async(id) => {
   const pokeRes = await fetch(`${pokeUrl}${id}`);
   const pokeInfo = await pokeRes.json();
@@ -16,9 +18,20 @@ const getPokemon = async(id) => {
   pokemon.attack = pokeInfo.stats[1].base_stat;
   pokemon.defense = pokeInfo.stats[2].base_stat;
   // pokemon.description = speciesInfo.flavor_text_entries; // THIS ONE NEEDS A HELPER FUCTION
-  
+  // pokemon.height = pokeInfo.height; // NEEDS HELPER TO CHANGE TO MY FORMAT
+  pokemon.hp = pokeInfo.stats[0].base_stat;
+  pokemon.isLegendary = speciesInfo.is_legendary;
+  pokemon.isMythical = speciesInfo.is_mythical;
+  pokemon.name = pokeInfo.name.split("-")[0];
+  pokemon.pictureUrl = pokeInfo.sprites.other['official-artwork'].front_default;
+  pokemon.specialAttack = pokeInfo.stats[3].base_stat;
+  pokemon.specialDefense = pokeInfo.stats[4].base_stat;
+  pokemon.speed = pokeInfo.stats[5].base_stat;
+  pokemon.weight = pokeInfo.weight; // NEEDS HELPER TO CHANGE TO MY FORMAT
 
-  console.log(pokemon);
+  const types = pokeInfo.types.map(type => type.type.name)
+
+  console.log(pokemon, types);
 }
 
 getPokemon(1)
