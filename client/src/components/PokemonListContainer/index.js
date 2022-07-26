12f33133270746f31ff5556f62fcca1c3e6bd61a
jeff <jlgranof@icloud.com>
2022-07-26 13:52:10 -0500

    prod cuild

--- client/src/components/PokemonListContainer/index.js
+++ client/src/components/PokemonListContainer/index.js
@@ -25,9 +25,9 @@ const PokemonListContainer = ({ pokemons }) => {
 
   return (
     <div className="text-2xl m-10">
-      <div>This will be the Search Bar</div>
+      {/* <div>This will be the Search Bar</div>
       <div>This will be the Advanced Search Container</div>
-      <div>This will be the Sort Container</div>
+      <div>This will be the Sort Container</div> */}
       <div 
         className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-6 gap-10"
       >
