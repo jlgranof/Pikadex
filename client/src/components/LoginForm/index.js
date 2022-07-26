3602cd549bb896abfc17ee3fad3ee9a4ae2c82b8
jeff <jlgranof@icloud.com>
2022-07-26 16:01:37 -0500

    got basic styling and demo users done for login page

--- client/src/components/LoginForm/index.js
+++ client/src/components/LoginForm/index.js
@@ -19,28 +19,66 @@ const LoginForm = () => {
     return dispatch(login({ username, password }));
   }
 
+  const demoUser = (e, user) => {
+    return dispatch(login({ username: user, password: 'password' }));
+  }
+
   return (
-    <form onSubmit={handleSubmit}>
-      <label>
-        Username 
-        <input 
-          type="text"
-          value={username}
-          onChange={e => setUsername(e.target.value)}
-          required
-        />
-      </label>
-      <label>
-        Password 
-        <input 
-          type="password"
-          value={password}
-          onChange={e => setPassword(e.target.value)}
-          required
-        />
-      </label>
-      <button type="submit">Log In</button>
-    </form>
+    <div className='flex flex-row h-screen justify-center'>
+      <div className='m-10 border-black border-2 rounded-lg h-1/2 bg-white'>
+        <form onSubmit={handleSubmit}>
+          <div className='flex flex-col m-10 items-center'>
+            <label className='m-5 text-xl'>
+              Username: 
+              <input 
+                className='rounded-lg m-2 border-slate-900 border-2'
+                type="text"
+                value={username}
+                onChange={e => setUsername(e.target.value)}
+                required
+              />
+            </label>
+            <label className='m-5 text-xl'>
+              Password: 
+              <input 
+                className='rounded-lg m-2 border-slate-900 border-2'
+                type="password"
+                value={password}
+                onChange={e => setPassword(e.target.value)}
+                required
+              />
+            </label>
+            <button 
+              type="submit"
+              className='m-5 border-slate-900 text-xl bg-slate-100 rounded-2xl'
+            >
+              Log In
+            </button>
+          </div>
+        </form>
+        <div className='mt-10 text-center text-xl'>
+          <p className='m-5'>Demo Users</p>
+          <div>
+            <button
+              onClick={(e) => demoUser(e, 'Ash')}
+              className='rounded-lg m-2 border-slate-900 border-2 p-2'
+            >Ash</button>
+          </div>
+          <div>
+            <button
+              onClick={(e) => demoUser(e, 'Brock')}
+              className='rounded-lg m-2 border-slate-900 border-2 p-2'
+            >Brock</button>
+          </div>
+          <div>
+            <button
+              onClick={(e) => demoUser(e, 'Misty')}
+              className='rounded-lg m-2 border-slate-900 border-2 p-2'
+            >Misty</button>
+          </div>
+        </div>  
+      </div>
+    </div>
   );
 };
 
